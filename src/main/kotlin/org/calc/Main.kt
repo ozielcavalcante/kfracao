01c9449f896e4b51733c2dfbe513230d1d6568d8
Oziel Alves Cavalcante <oziel@tjro.jus.br>
2023-10-30 18:47:45 -0400

    configura buld para fat jar

--- src/main/kotlin/org/calc/Main.kt
+++ src/main/kotlin/org/calc/Main.kt
@@ -1,26 +1,19 @@
-package org.calc;
+package org.calc
 
 import kotlin.system.exitProcess
 
-class Main {
-    fun fracao(texto: String): Fracao {
-        val vs = texto.split('/').map { t -> t.toInt() }
-        return if (vs.size > 1) Fracao(vs[0], vs[1]) else Fracao(vs[0])
-    }
-}
-
 fun main() {
-    val main = Main()
+    println("Calculadora de Frações")
     while (true) {
-        val entrada = readln().replace("\\s".toRegex(), "")
-        if (entrada.trim() == "s")
+        val entrada = readln().replace(" ", "")
+        if (entrada == "s")
             exitProcess(0)
 
         val args = entrada.split("(?=[-+*:])|(?<=[-+*:])".toRegex())
         try {
             val op = args[1]
-            val f1 = main.fracao(args[0])
-            val f2 = main.fracao(args[2])
+            val f1 = fracao(args[0])
+            val f2 = fracao(args[2])
 
             val resultado = when (op) {
                 "+" -> f1 + f2
@@ -37,3 +30,9 @@ fun main() {
         }
     }
 }
+
+fun fracao(texto: String): Fracao {
+    val vs = texto.split('/').map { t -> t.toInt() }
+    return if (vs.size > 1) Fracao(vs[0], vs[1]) else Fracao(vs[0])
+}
+
