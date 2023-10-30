660bd27900261eda1032a0e198cd4b6bb083a1cd
Oziel Alves Cavalcante <oziel@tjro.jus.br>
2023-10-30 07:12:15 -0400

    move classe e evita escape do menos

--- src/main/kotlin/org/calc/Main.kt
+++ src/main/kotlin/org/calc/Main.kt
@@ -1,18 +1,26 @@
-import org.calc.Fracao
+package org.calc;
+
 import kotlin.system.exitProcess
 
+class Main {
+    fun fracao(texto: String): Fracao {
+        val vs = texto.split('/').map { t -> t.toInt() }
+        return if (vs.size > 1) Fracao(vs[0], vs[1]) else Fracao(vs[0])
+    }
+}
+
 fun main() {
+    val main = Main()
     while (true) {
         val entrada = readln().replace("\\s".toRegex(), "")
         if (entrada.trim() == "s")
             exitProcess(0)
 
-        val args = entrada.split("(?=[+\\-*:])|(?<=[+\\-*:])".toRegex())
-        println(args)
+        val args = entrada.split("(?=[-+*:])|(?<=[-+*:])".toRegex())
         try {
             val op = args[1]
-            val f1 = fracao(args[0])
-            val f2 = fracao(args[2])
+            val f1 = main.fracao(args[0])
+            val f2 = main.fracao(args[2])
 
             val resultado = when (op) {
                 "+" -> f1 + f2
@@ -28,9 +36,4 @@ fun main() {
             println("entrada inválida: $m")
         }
     }
-}
-
-private fun fracao(texto: String): Fracao {
-    val vs = texto.split('/').map { t -> t.toInt() }
-    return if (vs.size > 1) Fracao(vs[0], vs[1]) else Fracao(vs[0])
 }
